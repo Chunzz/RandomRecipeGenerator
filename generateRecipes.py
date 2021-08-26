9309d5bc76ef77e3a7e370f0197920c5290a1ab8
fongcl <64897229+Chunzz@users.noreply.github.com>
2021-08-26 17:57:54 +1000

    Added random meals and generate

---
+++ generateRecipes.py
@@ -0,0 +1,12 @@
+from recipe import Recipe
+
+def generateRecipes():
+    recipeList = []
+
+    recipeList.append(Recipe("Meat Pie"))
+    recipeList.append(Recipe("Stroganoff"))
+    recipeList.append(Recipe("Butter Chicken"))
+    recipeList.append(Recipe("Larb"))
+    recipeList.append(Recipe("Japanese Curry"))
+
+    return recipeList
---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+from generateRecipes import generateRecipes
+
+while True:
+    # How many meals would you like to cook (1-5)?
+    numberOfMeals = int(input("\nHow many meals would you like to cook? (1-5): "))
+
+    #User Validation
+    if numberOfMeals > 5:
+        print("Too many meals entered, try a smaller number")
+    elif numberOfMeals <= 0:
+        print("Invalid number, try another number (1-5)")
+    else:
+        break
+
+
+#Generate random meals
+recipeList = generateRecipes()
+randomRecipeList = random.sample(recipeList, numberOfMeals)
+for recipe in randomRecipeList:
+    print(recipe.name)
+
+#Display GUI
